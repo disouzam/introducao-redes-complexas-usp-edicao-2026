1c3ee0fb2d27640966bc3be6e962f46930aa0d14
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-26 19:02:44 -0300

    Primeiro exemplo da aula do dia 26/01/2026
    - Timestamp: https://www.youtube.com/live/qzgSoFtH6xs?si=9v5ijdk5q6ED4o3x&t=3821

---
+++ 2026-01-26-Aula-01/exemplos-da-aula.py
@@ -0,0 +1,33 @@
+import marimo
+
+__generated_with = "0.19.6"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+@app.cell
+def _():
+    import networkx as nx
+    import matplotlib.pyplot as plt
+
+    # Número de nós
+    nodes = 10
+
+    # Criar um grafo completo (todos os nós conectados entre si)
+    G = nx.complete_graph(nodes)
+
+    # Desenhar o grafo
+    plt.figure(figsize=(6,6))
+    nx.draw(G, with_labels=True, node_color='lightblue', edge_color='gray', node_size=700, font_size=12)
+    plt.title(f"Grafo completo com {nodes} nós")
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    app.run()
