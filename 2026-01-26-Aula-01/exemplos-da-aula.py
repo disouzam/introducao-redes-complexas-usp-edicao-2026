fd09d8b3c68b55828b09080a54fff20ab2caa737
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-26 19:06:50 -0300

    Subdivisão das células para criar um grafo com número de nós controlado pelo usuário

--- 2026-01-26-Aula-01/exemplos-da-aula.py
+++ 2026-01-26-Aula-01/exemplos-da-aula.py
@@ -14,13 +14,25 @@ def _():
 def _():
     import networkx as nx
     import matplotlib.pyplot as plt
+    return nx, plt
 
+
+@app.cell
+def _():
     # Número de nós
     nodes = 10
+    return (nodes,)
 
+
+@app.cell
+def _(nodes, nx):
     # Criar um grafo completo (todos os nós conectados entre si)
     G = nx.complete_graph(nodes)
+    return (G,)
 
+
+@app.cell
+def _(G, nodes, nx, plt):
     # Desenhar o grafo
     plt.figure(figsize=(6,6))
     nx.draw(G, with_labels=True, node_color='lightblue', edge_color='gray', node_size=700, font_size=12)
