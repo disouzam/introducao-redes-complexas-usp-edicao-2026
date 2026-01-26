d84dd690edee986a7ce09c73e7abe9624b4f07c4
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-26 19:33:30 -0300

    Inclusão de célula com créditos aos professores `Camilo Rodrigues Neto` e `Masayuki Hase`
    - https://www.linkedin.com/in/camilo-rodrigues-neto-074aa265/
    - https://www.linkedin.com/in/masayuki-hase-3b26144/

--- 2026-01-26-Aula-01/exemplos-da-aula.py
+++ 2026-01-26-Aula-01/exemplos-da-aula.py
@@ -10,6 +10,18 @@ def _():
     return (mo,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Créditos:
+
+    O código apresentado nesse notebook foi inteiramente digitado por [Dickson Souza](https://www.linkedin.com/in/disouzam/) baseado na aula 01 do curso Introdução às Redes Complexas, ofertado pela USP, e ministrado pelos professores [Camilo Rodrigues Neto](https://www.linkedin.com/in/camilo-rodrigues-neto-074aa265/) e [Masayuki Hase](https://www.linkedin.com/in/masayuki-hase-3b26144/).
+
+    Introduzi algumas modificações para adaptação ao marimo, incluindo o uso de recursos interativos para seleção do número de nós usados para a criação do grafo completo.
+    """)
+    return
+
+
 @app.cell
 def _():
     import networkx as nx
